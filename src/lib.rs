e36acb244cad04f898703296c205423c3b4d8665
young <727988245@qq.com>
2022-05-02 09:49:24 +0800

    20220502

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,16 @@
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
     json_types::U128,
-    near_bindgen, Balance,
+    near_bindgen, Balance,PanicOnDefault,
     env, require, log, ext_contract, Promise, 
 };
-use rand::Rng;
 
 const TAX : f32 = 0.95;
 const FACTOR: u128 = 6;
 
-#[ext_contract(ext_ft)]
-trait ExtFT{
-    fn ft_transfer(&mut self, receiver_id: AccountId, amount: U128, memo:Option<String>);
-}
 
 #[near_bindgen]
-#[derive(BorshDeserialize, BorshSerialize)]
+#[derive(BorshDeserialize, BorshSerialize,PanicOnDefault)]
 pub struct Gamble {
 
     gamble_min_price : Balance,
@@ -24,25 +19,23 @@ pub struct Gamble {
 
 }
 
-impl Default for Gamble {
-    fn default() -> Self {
-        Self {
-            gamble_max_price : 0,
-            gamble_min_price : 0,
-        }
-    }
-}
 
 #[near_bindgen]
 impl Gamble {
-
+    
     #[init]
     pub fn new() -> Self {
+        
+        let account_balance = env::account_balance();
+        let gamble_max_price = account_balance / (5 * FACTOR);
+        log!("we have {} uints in total, be sure not to exceed the max gamble price limit {} to get {}X \n", account_balance, gamble_max_price, FACTOR);
+
         Self{
-            gamble_max_price : 0,
+            gamble_max_price : gamble_max_price,
             gamble_min_price : 0,
         }
     }
+
 
     pub fn get_minimal_gamble_price(&self) -> u128 {
         self.gamble_min_price
@@ -60,7 +53,7 @@ impl Gamble {
     pub fn update_price(&mut self){
         let account_balance = env::account_balance();
         self.gamble_max_price = account_balance / (5 * FACTOR);
-        log!("we have {} uints in total, be sure not to exceed the max gamble price limit {} to get {}X", account_balance, self.gamble_max_price, FACTOR);
+        log!("we have {} uints in total, be sure not to exceed the max gamble price limit {} to get {}X \n", account_balance, self.gamble_max_price, FACTOR);
     }
 
 
@@ -68,32 +61,32 @@ impl Gamble {
     pub fn sponsor(&mut self){
         let sponsor_id = env::signer_account_id();
         let deposit = env::attached_deposit();
-        log!("sponsor {} has add {} to the game to increase balance, thank you ~", sponsor_id, deposit);
+        log!("sponsor {} has add {} to the game to increase balance, thank you ~ \n", sponsor_id, deposit);
         self.update_price();
     }
 
     #[payable]
-    pub fn gamble(&mut self) -> u128{
+    pub fn gamble(&mut self) -> u8{
         let gambler_id = env::signer_account_id();
         let deposit = env::attached_deposit();
 
         require!(deposit>=self.gamble_min_price,"The gamble price must exceed gamble_min_price");
         require!(deposit<=self.gamble_max_price,"The gamble price must not exceed gamble_max_price");
         
-        let num = self.rand();
+        let num = self.rand_dice();
 
-        if num == FACTOR {
+        if num == FACTOR as u8 {
             let amount = (deposit as f32 ) *(FACTOR as f32) * TAX;
             let amount_u128 = amount  as u128;
-            log!("Congratuations to {}, he has won the gamble, the prize is {}",gambler_id,deposit);
+            log!("Congratuations to {}, he has won the gamble, the prize is {} \n",gambler_id,deposit);
             Promise::new(gambler_id).transfer(amount_u128);
         }
         self.update_price();
         return num;
     }
 
-    pub fn rand(&self) -> u128 {
-        rand::thread_rng().gen_range(1..FACTOR+1)    
+    pub fn rand_dice(&self) -> u8 {
+        *env::random_seed().get(0).unwrap()%6+1
     }
 
 }
@@ -116,12 +109,12 @@ mod tests {
             input,
             block_index: 0,
             block_timestamp: 0,
-            account_balance: 0,
+            account_balance: 222,
             account_locked_balance: 0,
             storage_usage: 0,
             attached_deposit: 0,
             prepaid_gas: Gas(10u64.pow(18)),
-            random_seed: [0u8; 32],
+            random_seed: [5u8; 32],
             view_config: None,
             output_data_receivers: vec![],
             epoch_height: 19,
@@ -136,12 +129,11 @@ mod tests {
             gamble_min_price : 0,
             gamble_max_price : 0,
         };
-        for _ in 0..100{
-            let val = contract.rand();
-            // println!("{}",val);
-            assert_eq!(val>=1,true,"The random value should not be smaller than 1");
-            assert_eq!(val<=6,true,"The random value should not be bigger than 6");
-        }
+        let val = contract.rand_dice();
+        println!("{}",val);
+        assert_eq!(val>=1,true,"The random value should not be smaller than 1");
+        assert_eq!(val<=6,true,"The random value should not be bigger than 6");
+        
     }
 
     #[test]
@@ -156,8 +148,6 @@ mod tests {
         println!("maximum : {}",contract.get_maximum_gamble_price());
         println!("balance : {}",contract.get_balance());
         println!("gamble: {}",contract.gamble());
-
-
     }
 }
 
